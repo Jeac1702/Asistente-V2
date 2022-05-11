82ae6818b5bbcc987c61c192a5cc0aac5e2b0cf7
juancatap7 <102172377+juancatap7@users.noreply.github.com>
2022-05-11 00:14:13 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -2,10 +2,10 @@
 $servidor= "localhost";
 $usuario= "root";
 $password = "";
-$nombreBD= "php_BD";
+$nombreBD= "mydb";
 $db = new mysqli($servidor, $usuario, $password, $nombreBD);
 if ($db->connect_error) {
-    die("la conexión ha fallado: " . $db->connect_error);
+    die("la conexiÃ³n ha fallado: " . $db->connect_error);
 }
 if (!$db->set_charset("utf8")) {
     printf("Error al cargar el conjunto de caracteres utf8: %s\n", $db->error);
